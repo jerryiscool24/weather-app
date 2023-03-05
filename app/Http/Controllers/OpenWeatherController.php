2e6fdaf3f5b47b92b78dd2cd719ebaa4ab9baf75
Jerry Manulat <jerrymanulat03@gmail.com>
2023-03-05 23:56:56 +0800

    Integrate open weather api

---
+++ app/Http/Controllers/OpenWeatherController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Core\OpenWeather\Weather;
+use App\Http\Requests\WeatherRequest as Request;
+use App\Http\Resources\WeatherResource as Resource;
+use Symfony\Component\HttpFoundation\Response;
+
+class OpenWeatherController extends Controller
+{
+    /**
+     * Handle the incoming request.
+     */
+    public function __invoke(Request $request)
+    {
+        $service = new Weather();
+
+        $response = $service->get('weather', ['q' => $request->get('city')]);
+
+        if($response->cod === 200) {
+            return new Resource($response);
+        }
+
+        return response()->json($response, Response::HTTP_BAD_REQUEST);
+    }
+}
